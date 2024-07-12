db3839046756a05270ab7e14673bffcd3ffca00f
Chinmaya Garg <ch745692@dal.ca>
2024-07-12 13:13:11 +0530

    Fix

--- T5_garg_chinmaya_B00925398_5709/settings.py
+++ T5_garg_chinmaya_B00925398_5709/settings.py
@@ -128,5 +128,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-import django_heroku
-django_heroku.settings(locals())
